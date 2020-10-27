97e1f2ef4edebf9934aecd81efeabc6a96aef176
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-10-27 14:34:17 +0100

    initial preparation for Endpoint Class - constructor is done

--- demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
+++ demo/demo/src/main/java/com/example/demo/endpoints/Endpoint.java
@@ -1,5 +1,20 @@
 package com.example.demo.endpoints;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.services.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RestController
+@RequestMapping("/restapi/")
 public class Endpoint {
+    private Service service;
+
+    //wstrzykiwanie obiektu Service
+    @Autowired
+    public Endpoint(Service service) {
+        this.service = service;
+    }
 
 }
