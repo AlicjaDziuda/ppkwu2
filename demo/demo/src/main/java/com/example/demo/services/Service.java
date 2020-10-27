8c5fcbc5ea6067ff971b9317f78f51d4add0b8eb
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-10-27 14:38:41 +0100

    outline of service for recognizing characters

--- demo/demo/src/main/java/com/example/demo/services/Service.java
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -2,5 +2,7 @@ package com.example.demo.services;
 @org.springframework.stereotype.Service
 
 public class Service {
-
+    public String reverseCharacters(String characters) {
+        return characters;
+    }
 }
