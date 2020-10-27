cf7823b42532b708660526966515c849962581fc
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-10-27 15:00:56 +0100

    Service can verify if string contains digit number

--- demo/demo/src/main/java/com/example/demo/services/Service.java
+++ demo/demo/src/main/java/com/example/demo/services/Service.java
@@ -3,6 +3,19 @@ package com.example.demo.services;
 
 public class Service {
     public String recognizeCharacters(String characters) {
-        return characters;
+        int digitCounter = 0;
+        for( int i =0 ; i < characters.length(); i++){
+            Boolean flag = Character.isDigit(characters.charAt(i));
+            switch (flag.toString()) {
+                case "true":
+                    digitCounter = digitCounter +1;
+                    break;
+                case "false":
+                    break;
+                default:
+            }
+        }
+
+        return String.valueOf(digitCounter);
     }
 }
